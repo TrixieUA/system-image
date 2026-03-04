478876574b350883dfa234af690ad04478ec829f
TrixieUA <ldelovka4@gmail.com>
2026-03-04 11:59:23 +0200

    Update updates-testing.sh

--- files/scripts/updates-testing.sh
+++ files/scripts/updates-testing.sh
@@ -5,7 +5,5 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-
-sed -i '/^\[updates-testing\]/,/^\[/ {s/^enabled=0$/enabled=1/}' '/etc/yum.repos.d/fedora-updates-testing.repo'
-
+dnf config-manager --enable updates-testing -y
 dnf update -y --refresh
